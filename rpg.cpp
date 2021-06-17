82bcf59d1940cf56b6094e177d38e98969e60295
BrookSK <lucasrvacari99@gmail.com>
2021-06-17 18:33:37 -0300

    criei a fase Trapaceiros e editei a fase da cidade.

--- rpg.cpp
+++ rpg.cpp
@@ -5,17 +5,11 @@
 
 /**
  * Um sistema de RPG em modo text que, dependendo das 
- * escolhas da/do jogadora/or pode mudar o final
+ * escolhas da/do jogadora/or pode mudar o final.
  */
 
-// Fazer uma forma do personagem escolher suas raça
-
-
-//vamos
-//teste on
-//ok
-//ll
-
+// TODO Fazer uma forma do jogador ou jogadora escolher sua raça
+// TODO Sendo que cada raça tem a possibilidade de  ter mais ou menos atributos que as outras
 
 struct Personagem{
     std::string nome;
@@ -34,7 +28,7 @@ float geraAleatorio(){
 struct Personagem boasVindasDoJogo(){
     std::cout << "Boas vindas ao nosso mundo!\n";
     std::cout << "Um sentimento de obscuridade tomou conta de nossa terra, \n";
-    std::cout << "o rei Demônio dominou as principais formas de defesa do nosso povo\n";
+    std::cout << "o rei Demônio dominou as principais forças de defesa do nosso povo\n";
     std::cout << "e a cada dia que passa as esperanças decaem mais e mais...\n";
 
     std::cout << "Como somos um bando de incompetentes e não conseguimos nos defender\n";
@@ -54,21 +48,27 @@ struct Personagem boasVindasDoJogo(){
     eu.altura = geraAleatorio() * 300;
     eu.forcaFisica = geraAleatorio() * 100;
     eu.forcaMagica = geraAleatorio() * 50;
-    eu.creditos = 26;
+    eu.creditos = 38;
 
     return eu;
 }
 
 void mostraStatusDoJogador(struct Personagem &p){
+
+    if(p.energia == 0){
+        std::cout << "Você morreu!!!" << std::endl;
+        return;
+    }
+
     std::cout << "Você agora tem " << p.altura/100.0 <<
-                " metros de altura, " << p.energia <<" de energia com uma forma de intensidade " <<
+                " metros de altura, " << p.energia << " de energia com uma força de intensidade " <<
                 p.forcaFisica << " e um poder mágico de " <<
-                p.forcaMagica << " orbites $" << p.creditos << std::endl;
+                p.forcaMagica << " com $" << p.creditos << " orbites" << std::endl;
 }
 
 void faseDaCidade(struct Personagem &p){ // &p referencia à struct "Personagem"
     
-    unsigned int opcao = -1;
+    unsigned int opcao = 0;
 
     while(opcao < 1 || opcao > 2){
         std::cout << "Você está na rua principal da cidade, nesta rua você vê dois estabelecimentos: Uma quitanda de um lado uma loja de armas do outro.\n Para onde você deseja ir?" << std::endl;
@@ -84,36 +84,35 @@ void faseDaCidade(struct Personagem &p){ // &p referencia à struct "Personagem"
         std::cin >> opcao;
 
         if(opcao == 2){
-            
+
             opcao = 0;
 
             while(opcao < 1 || opcao > 2){
-                std::cout << "Qual alimento desejas?" << std::endl;
-                std::cout << "1-Macarrão (Dá muita energia)\n2-Balinha (Dá pouca energia) " << std::endl;
-
+                std::cout << "1-Macarrão (Dá muita energia) - $50\n2-Balinha (Dá pouca energia) - $10" << std::endl;
                 std::cin >> opcao;
-
-                switch(opcao){
-                    case 1:
-                        if(p.creditos >= 50){
-                            p.energia += 50;
-                            p.creditos -= 50;
-                        }else{
-                            std::cout << "Desculpe mas não dá..." << std::endl;
-                        }
-                        break;
-                    case 2:
-                        if(p.creditos >= 10){
-                            p.energia += 10;
-                            p.creditos -= 10;
-                        }else{
-                            std::cout << "Desculpe mas não dá..." << std::endl;
-                        }
-                        break;
-                }
-
-                return;
             }
+
+            switch(opcao){
+                case 1:
+                    if(p.creditos >= 50){
+                        p.energia += 50;
+                        p.creditos -= 50;
+                        std::cout << "Foi bom enquanto durou..." << std::endl;
+                    }else{
+                        std::cout << "Desculpe mas não posso te vender isso..." << std::endl;
+                    }
+                    break;
+                case 2:
+                    if(p.creditos >= 10){
+                        p.energia += 10;
+                        p.creditos -= 10;
+                        std::cout << "Foi rapido..." << std::endl;
+                    }else{
+                        std::cout << "Desculpe mas não posso te vender isso..." << std::endl;
+                    }
+                    return;
+            }
+            return;
 
         }else if(opcao == 3){
             std::cout << "Prepare-se para morrer indolente!!!" << std::endl;
@@ -121,7 +120,6 @@ void faseDaCidade(struct Personagem &p){ // &p referencia à struct "Personagem"
             return;
         }else{
             std::cout << "Que cara estranho....";
-            return;
         }
 
     }
@@ -129,44 +127,51 @@ void faseDaCidade(struct Personagem &p){ // &p referencia à struct "Personagem"
     if(opcao == 2){
         std::cout << "O vendedor te olha e pergunta: Pronto pra batalha?" << std::endl;
         
-        std::cout << "1-Não\n2-Espada\n3-Adaga\n4-Arco e flecha" << std::endl;
+        std::cout << "1-Não\n2-Espada - $200\n3-Adaga - $25\n4-Arco e flecha - $120" << std::endl;
         std::cin >> opcao;
 
         if(opcao == 2){
 
+            std::cout << "Bela escolha!" << std::endl;
+
             if(p.creditos >= 200){
-                std::cout << "Bela escolha!" << std::endl;
-                std::cout << "Muitos cairão por esta lámina!!" << std::endl;
+                std::cout << "Muitos cairão por essa lâmina!!" << std::endl;
                 p.creditos = p.creditos - 200;
                 p.armamento = "Espada";
+
                 mostraStatusDoJogador(p);
-            }else
+            }else{
                 std::cout << "Você não merece este tesouro plebeu!!" << std::endl;
+            }
+
         }else if(opcao == 3){
-            
             std::cout << "Pelo menos dá para apontar um lápis" << std::endl;
 
             if(p.creditos >= 25){
                 std::cout << "ok..." << std::endl;
                 p.creditos = p.creditos - 25;
                 p.armamento = "Adaga";
+
                 mostraStatusDoJogador(p);
-            }else;
-            
-        }else if(opcao == 4){
-            if(p.creditos >= 120){
+            }else{
                 std::cout << "Saia daqui farrapo!!!" << std::endl;
-                std::cout << "Uhm..." << std::endl;
+            }
+        }else if(opcao == 4){
+             if(p.creditos >= 120){
+                 std::cout << "Uhm..." << std::endl;
                 p.creditos = p.creditos - 120;
                 p.armamento = "Arco e flecha";
+                
                 mostraStatusDoJogador(p);
-            }else
+             }else{
                 std::cout << "Saia daqui seu pobre!" << std::endl;
+                }
+        }
         }else{
             std::cout << "Vá embora!." << std::endl;
         }
     }
-}
+
 
 void faseTrapaceiros(struct Personagem &p){ // &p referencia à struct "Personagem"
         unsigned int opcao = -1;
@@ -178,51 +183,114 @@ void faseTrapaceiros(struct Personagem &p){ // &p referencia à struct "Personag
         }
 
         if(opcao == 1){
-
             if(p.armamento == ""){
                 std::cout << "Você vai na mão contra os trapaceiros..." << std::endl;
                 p.creditos = p.creditos - p.creditos;
-                std::cout << "Você foi roubado e agora possui " << p.creditos << " créditos" << std::endl;
+                std::cout << "Você foi roubado e agora possui " << "$"<< p.creditos << " orbites"<< std::endl;
+                
+                if(p.creditos <=5){
+                    std::cout << "Você sai andando desanimado, e encontra uma mulher pela qual, deixou cair uma joia no chão." << std::endl;
+                    std::cout << "Você tem duas alternativas:" << std::endl;
+                    std::cout << "1-Devolver para ela\n2-Roubar a joia" << std::endl;
+                    std::cin >> opcao;
+                    
+                    switch(opcao){
+                        case 1:
+                            std::cout << "Você grita para a ela e a devolve a joia." << std::endl;
+                            std::cout << "Ela muito agradescida lhe-da uma recompensa de $26 orbites" << std::endl;
+                            p.creditos = p.creditos + 26;
+                            
+                            mostraStatusDoJogador(p);
+                            
+                            std::cout << "Então tu poderá voltar para a cidade ou continuar vossa jornada..." << std::endl;
+                            std::cout << "1-voltar para a cidade\n2-continuar jornada" << std::endl;
+                            std::cin >> opcao;
+                            
+                            if(opcao == 1){
+                                faseDaCidade(p);
+                            }else if(opcao == 2){
+                                std::cout << "Tu continuas vosso caminho..." << std::endl;
+                            }return;
+                        case 2:
+                            std::cout << "Você pega a joia e vai para um comércio." << std::endl;
+                            std::cout << "O vendedor te olha e pergunta: O que tens para mim meu jovem ?" << std::endl;
+                            std::cout << "Tu mostra para ele a joia e o comerciante te faz duas ofertas:" << std::endl;
+                            std::cout << "1-Compro esta joia de ti por $30 Orbites\n2-Compro esta joia de ti por uma poção de Força Mágica" << std::endl;
+                            std::cin >> opcao;
+                            if(opcao == 1){
+                                p.creditos = p.creditos + 30;
+                                
+                                mostraStatusDoJogador(p);
+                                
+                                std::cout << "Então tu poderá voltar para a cidade ou continuar vossa jornada..." << std::endl;
+                                std::cout << "1-voltar para a cidade\n2-continuar jornada" << std::endl;
+                                std::cin >> opcao;
+                                
+                                if(opcao == 1){
+                                    faseDaCidade(p);
+                                }else if(opcao == 2){
+                                    std::cout << "Tu continuas vosso caminho..." << std::endl;
+                                }return;
+                                
+                            }else if(opcao == 2){
+                                p.forcaMagica = p.forcaMagica + 9;
+                                
+                                mostraStatusDoJogador(p);
+                                
+                                std::cout << "Então tu poderá voltar para a cidade ou continuar vossa jornada..." << std::endl;
+                                std::cout << "1-voltar para a cidade\n2-continuar jornada" << std::endl;
+                                std::cin >> opcao;
+                                
+                                if(opcao == 1){
+                                    faseDaCidade(p);
+                                }else if(opcao == 2){
+                                    std::cout << "Tu continuas vosso caminho..." << std::endl;
+                                }return;
+                                
+                            }break;
+                        default:
+                            std::cout << "Erro" << std::endl;
+                        break;
+                    }
+                }
+                
             }else{
                 std::cout << "Tu sacas vosso(a) " << p.armamento << " e intimida os trapaceiros" << std::endl;
                 std::cout << "Eles não se intimidam e partem para cima. Tu tens a oportunidade de matá-los, o que tu farás?" << std::endl;
                 std::cout << "1-Os Matarei\n2-Os Imobilizarei" << std::endl;
                 std::cin >> opcao;
+
                 if(opcao == 2){
-                    
                     opcao = 0;
 
                     while(opcao < 1 || opcao > 2){
                         std::cout << "A partir de qual parte do corpo desejas tu imobilizar os dois trapaceiros?" << std::endl;
                         std::cout << "1-A partir da cabeça\n2-A partir das pernas" << std::endl;
-
                         std::cin >> opcao;
 
                         switch(opcao){
                             case 1:
-                                if(p.creditos >= 30){
+                                if(p.forcaFisica >= 30){
                                     p.energia -= 70;
                                     p.creditos -= 20;
+                                    std::cout << "Você os imobilizou e continuou a vossa jornada" << std::endl;
                                 }else{
                                     std::cout << "Desculpe, mas não dá..." << std::endl;
-                                }
-                                break;
+                                }break;
                             case 2:
-                                if(p.creditos >= 15){
+                                if(p.forcaFisica >=15){
                                     p.energia += 50;
                                     p.creditos -= 10;
+                                    std::cout << "Você os imobilizou e continuou a vossa jornada" << std::endl;
                                 }else{
                                     std::cout << "Desculpe, mas não dá..." << std::endl;
-                                }
-                                break;
+                                }break;
                             default:
                                 std::cout << "Erro" << std::endl;
-                                break;
+                            break;
                         }
-
                         return;
                     }
-
                 }else if(opcao == 1){
                     std::cout << "Preparem-se para morrerem, indolentes!!!" << std::endl;
                     p.energia = p.energia - p.energia;
@@ -232,7 +300,7 @@ void faseTrapaceiros(struct Personagem &p){ // &p referencia à struct "Personag
                     std::cout << "Não farás nada? (...)";
                     return;
                 }
-        
+            }
         }if(opcao == 2){
             std::cout << "Você então decidiu fujir? foi um erro!" << std::endl;
             std::cout << "Você infelizmente tropeça e é capturado pelos dois trapaceiros. O que farás agora?" << std::endl;
@@ -241,17 +309,11 @@ void faseTrapaceiros(struct Personagem &p){ // &p referencia à struct "Personag
             std::cin >> opcao;
 
             if(opcao == 1){
-
                 std::cout << "Eles nunca lhe darão redenção!" << std::endl;
-
-            }else if(opcao == 2){
-                
-                std::cout << "Impossível! Você está preso!" << std::endl;
-            
             }else if(opcao == 3){
                 std::cout << "Você sorrateiramente puxa uma faquinha escondida em suas vestes, infelizmente um trapaceiro percebe e te nocauteia" << std::endl;
 
-                if(p.creditos >= 60){
+                if(p.energia >= 60){
                     std::cout << "Você perdeu 60 pontos de energia!..." << std::endl;
                     p.energia = p.energia - 60;
 
@@ -259,13 +321,14 @@ void faseTrapaceiros(struct Personagem &p){ // &p referencia à struct "Personag
                 }else{
                     std::cout << "Você infelizmente morreu!!!" << std::endl;
                 }
+            }else if(opcao == 2){
+                std::cout << "Ao fugir, você chega até um desfiladeiro, um local sem saida, escorrega e cai..." << std::endl;
             }else{
                 std::cout << "Não fará nada?" << std::endl;
             }
-        } 
-    }
-}
+        }
 
+}
 
 int main(int argc, char** argv)
 {
@@ -279,7 +342,9 @@ int main(int argc, char** argv)
     std::cout << "Lembre-se que nem sempre grandes poderes significam grande honra!\n";
 
     mostraStatusDoJogador(j);
+
     faseDaCidade(j);
+
     faseTrapaceiros(j);
 
     mostraStatusDoJogador(j);
